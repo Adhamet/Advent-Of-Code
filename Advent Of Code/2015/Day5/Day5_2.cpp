0bfda50f8b1d2781163237e70f1a99a002b1461f
Adham Essam <98621817+Adhamet@users.noreply.github.com>
2022-02-05 00:27:46 +0200

    Update Day5_2.cpp

--- Advent Of Code/2015/Day5/Day5_2.cpp
+++ Advent Of Code/2015/Day5/Day5_2.cpp
@@ -6,26 +6,38 @@ int main()
 {
     ifstream INPUT;
     INPUT.open("Day5_Input.txt");
-    int prop1=0,prop2=0,nice=0;
+    int prop1=0,prop2=0,nice=0, row, col, col1;
     char input[1000][16];
-    for ( int row = 0; row < 1000; row++ )
+    for ( row = 0; row < 1000; row++ )
     {
-        for ( int col = 0; col < 16; col++ )
+        for ( col = 0; col < 16; col++ )
         {
             INPUT >> input[row][col];
+            input[row][col1] == input[row][col];
+            col1++;
         }
-        for ( int col = 0; col < 16; col++ )
+        for ( col = 0; col < 16; col++ )
         {
-            if ( input[row][col] == input[row][col+2] && input[row][col+1] == input[row][col+3] && input[row][col] != input[row][col+1] && input[row][col+2] != input[row][col+1])
+            for ( col1 = 0; col1 < 16; col1++ )
             {
-                prop1++;
+                    if ( input[row][col1+2] == input[row][col] && input[row][col1+3] == input[row][col+1] && input[row][col1+2] != input[row][col])
+                    {
+                        if (col1+1 < 16 && col1+2 < 16 && col1+3 < 16)
+                        {
+                            prop1++;
+                        }
+                    }
             }
         }
-        for ( int col = 0; col < 16; col++ )
+        
+        for ( col = 0; col < 16; col++ )
         {
             if ( input[row][col] == input[row][col+2] )
             {
-                prop2++;
+                if (input[row][col+2] < 16)
+                {
+                    prop2++;
+                }
             }
         }
         if ( prop1 > 1 && prop2 != 0 )
