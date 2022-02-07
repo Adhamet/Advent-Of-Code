2493f5e0570583361b83e68357d582592aa38b44
Adham Essam <98621817+Adhamet@users.noreply.github.com>
2022-02-07 19:54:53 +0200

    Update Day4.py

--- Advent Of Code/2015/Day4/Day4.py
+++ Advent Of Code/2015/Day4/Day4.py
@@ -1,6 +1,21 @@
+#Part 1
+
 import hashlib
 
+hashNum = 0
 
-result = hashlib.md5(b"yzbqklnj")
+while(True):
+    hashNumBytes = bytes(str(hashNum) , 'utf-8')
+    bencryption = hashlib.md5(b"yzbqklnj"+hashNumBytes)
+    aencryption = bencryption.hexdigest()
 
-print(result.hexdigest())
+    if aencryption[0:5] == "00000":
+        print(hashNum)
+        break
+    else: 
+        hashNum += 1 
+        if (hashNum % 10000 == 0):
+            print(hashNum)
+
+
+#Part 2, just change the range of aencryption to 0:6 and add a 0 in its equivalent.
