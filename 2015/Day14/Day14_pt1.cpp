a875a611505c26d51a3c78ccd2eb7a52bd391317
Adham Essam <98621817+Adhamet@users.noreply.github.com>
2022-03-16 22:39:19 +0200

    Add files via upload

---
+++ 2015/Day14/Day14_pt1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define numR 9
+#define elapsedT 2503
+
+using namespace std;
+
+struct reindeer{
+    string name;
+    int speed, maxDist = 0, timeRun, timeRest;
+};
+
+int main()
+{
+    // Declaration and Initialization Section -----------------------------------------------------------
+    reindeer reinDeer[numR]; 
+    string temp; 
+    int i = 0;
+    
+    ifstream inData; 
+    inData.open("Day14_Input.txt");
+    
+    // Input Section ------------------------------------------------------------------------------------
+    while(inData.eof() == 0)
+    {
+        inData >> reinDeer[i].name >> temp >> temp >> reinDeer[i].speed >> temp >> temp >> reinDeer[i].timeRun 
+               >> temp >> temp >> temp >> temp >> temp >> temp >> reinDeer[i].timeRest >> temp;
+        i++;
+    }
+
+    // Algorithm Section --------------------------------------------------------------------------------
+    for (int i = 0; i < numR; i++)
+    {
+        for (int j = 1; j <= elapsedT; j++)
+        {
+            reinDeer[i].maxDist += reinDeer[i].speed;
+            if (reinDeer[i].maxDist % (reinDeer[i].speed * reinDeer[i].timeRun) == 0)
+                j += reinDeer[i].timeRest;
+        }
+    }
+
+    // Output Section ----------------------------------------------------------------------------------
+    int winner = 0;
+    for (int i = 0; i < numR; i++)
+    {
+        if (reinDeer[i].maxDist > winner)
+        {
+            winner = reinDeer[i].maxDist;
+        }
+    }
+    cout << winner;
+}
---
+++ 2015/Day14/Day14_pt2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define numR 9
+#define elapsedT 2503
+
+using namespace std;
+    
+struct reindeer{
+    string name;
+    int speed, maxDist = 0, timeRun, timeRest, score = 0, last = 2503, restLast = 0;
+};
+
+int main()
+{
+    // Declaration and Initialization Section -----------------------------------------------------------
+    ifstream inData; 
+    inData.open("Day14_Input.txt");
+    
+    reindeer reinDeer[numR]; 
+    string temp; 
+    int i = 0;
+
+    // Input Section ------------------------------------------------------------------------------------
+    while(inData.eof() == 0)
+    {
+        inData >> reinDeer[i].name >> temp >> temp >> reinDeer[i].speed >> temp >> temp >> reinDeer[i].timeRun 
+               >> temp >> temp >> temp >> temp >> temp >> temp >> reinDeer[i].timeRest >> temp;
+        i++;
+    }
+
+    // Algorithm Section --------------------------------------------------------------------------------
+    for (int i = 1; i < elapsedT; i++)
+    {
+        for(int j = 0; j < numR; j++)
+        {
+            if (reinDeer[j].restLast < i)
+            {
+                reinDeer[j].maxDist += reinDeer[j].speed;
+                if (reinDeer[j].maxDist % (reinDeer[j].speed * reinDeer[j].timeRun) == 0)
+                    reinDeer[j].restLast = i + reinDeer[j].timeRest;
+            }
+        }
+
+
+        int winner = 0;
+        for (int j = 0; j < numR; j++)
+            if (winner < reinDeer[j].maxDist)
+                winner = reinDeer[j].maxDist;
+
+        for (int j = 0; j < numR; j++)
+            if (reinDeer[j].maxDist == winner) 
+                reinDeer[j].score++; 
+    }
+
+    // Output Section ----------------------------------------------------------------------------------
+    int gold = 0;
+    for (int i = 0; i < numR; i++)
+    {
+        if (reinDeer[i].score > gold)
+        {
+            gold = reinDeer[i].score;
+        }
+    }
+    cout << gold;
+}
